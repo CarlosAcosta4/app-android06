54bb1379ede0b5e9d296fdc8f8b56fb0db453966
CarlosAcosta4 <carlosacosta4820@gmail.com>
2023-11-06 18:54:01 -0500

    first commit

---
+++ app/src/main/java/com/example/apprecycleviewimage/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.apprecycleviewimage;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.os.Bundle;
+
+import com.example.apprecycleviewimage.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private AndroidAdapter androidAdapter = new AndroidAdapter();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.rvandroid.setLayoutManager(
+                new LinearLayoutManager(getApplicationContext())
+        );
+        binding.rvandroid.setAdapter(androidAdapter);
+        androidAdapter.setData(getData());
+    }
+
+    private ArrayList<Android> getData(){
+        ArrayList<Android> list = new ArrayList<>();
+        list.add(new Android(list.size() +1,
+                R.drawable.image1,
+                "Angel cake",
+                "Android 1.0"));
+
+        list.add(new Android(list.size() +1,
+                R.drawable.image2,
+                "Angel cake",
+                "Android 1.0"));
+
+        list.add(new Android(list.size() +1,
+                R.drawable.image3,
+                "Angel cake",
+                "Android 1.0"));
+
+        list.add(new Android(list.size() +1,
+                R.drawable.image4,
+                "Angel cake",
+                "Android 1.0"));
+
+        list.add(new Android(list.size() +1,
+                R.drawable.image5,
+                "Angel cake",
+                "Android 1.0"));
+
+        list.add(new Android(list.size() +1,
+                R.drawable.image6,
+                "Angel cake",
+                "Android 1.0"));
+
+        list.add(new Android(list.size() +1,
+                R.drawable.image7,
+                "Angel cake",
+                "Android 1.0"));
+
+        list.add(new Android(list.size() +1,
+                R.drawable.image8,
+                "Angel cake",
+                "Android 1.0"));
+
+        return list;
+    }
+}
